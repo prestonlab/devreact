f1fe5d7fe19268952c2782ce0484afcf512008b4
Neal Morton <mortonne@gmail.com>
2022-08-22 12:32:39 -0500

    refactor: move kidrep read function

--- src/devreact/__init__.py
+++
@@ -1,1 +0,0 @@
-from devreact import rt
--- src/devreact/task.py
+++ src/devreact/task.py
@@ -40,3 +40,15 @@ def read_remind(data_file):
         }
     )
     return data
+
+
+def read_kidrep(csv_file):
+    """Read response time data."""
+    data = pd.read_csv(csv_file)
+    data['response_time'] /= 1000
+    data.trial_type = data.trial_type.map({1: 'AB', 2: 'BC', 3: 'XY', 4: 'AC'})
+    data = data.astype({'trial_type': 'category'})
+    data.trial_type.cat.reorder_categories(
+        ['AB', 'XY', 'BC', 'AC'], ordered=True, inplace=True
+    )
+    return data
